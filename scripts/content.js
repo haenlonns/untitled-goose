f40539cbea38c96fac3737a805f8ff34b28a4793
kevinzhangTT <kevin9019888@gmail.com>
2024-09-14 20:21:42 -0400

    make image move

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,55 @@
-const article = document.querySelector("article");
+import goose from './../img/1.2_default_run.gif';
 
-const image = document.createElement("img");
-image.src = "https://www.w3schools.com/images/w3schools_green.jpg"; // Replace with the actual relative path to your image
-image.alt = "Goose GIF"; // Provide an appropriate alt text
+const article = document.querySelector('article');
+
+const image = document.createElement('img');
+// image.src = 'https://cdnb.artstation.com/p/assets/images/images/050/026/891/original/luiz-aquino-playing-exp.gif?1653908904';
+image.src = goose;
+image.alt = 'GOOOSE RUNNINGGGG'; // Provide an appropriate alt text
+
+// set css to make the position absolute and draggable
+image.style.position = 'absolute';
+image.style.top = '100px';
+image.style.left = '100px';
+image.style.width = '150px';
+image.style.cursor = 'move';
+image.style.zIndex = '999';
+
+image.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+    
+    // get cursor and image pos
+    let shiftX = event.clientX - image.getBoundingClientRect().left;
+    let shiftY = event.clientY - image.getBoundingClientRect().top;
+
+    // fucking jank lol
+    function moveAt(pageX, pageY) {
+        image.style.left = pageX - shiftX + 'px';
+        image.style.top  = pageY - shiftY + 'px';
+    }
+
+    // move the image
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
+
+    // thanks gpt老师
+    document.addEventListener('mousemove', onMouseMove);
+
+    // remove listener when mouse is up
+    image.addEventListener('mouseup', () => {
+        document.removeEventListener('mousemove', onMouseMove);
+    });
+});
+
+// ???
+image.ondragstart = function() {
+    return false;
+};
 
 // Support for API reference docs
-const heading = article.querySelector("h1");
+const heading = article.querySelector('h1');
 // Support for article docs with date
-const date = article.querySelector("time")?.parentNode;
+const date = article.querySelector('time')?.parentNode;
 
-(date ?? heading).insertAdjacentElement("afterend", image);
+(date ?? heading).insertAdjacentElement('afterend', image);
